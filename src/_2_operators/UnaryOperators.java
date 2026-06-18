c0bbd0b7ac6aa7d1ceefa05bce46b7895dfcc65d
pavankumarreddyc08 <pavankumarreddyc08@gmail.com>
2026-06-18 22:34:14 +0530

    added operator examples

---
+++ src/_2_operators/UnaryOperators.java
@@ -0,0 +1,19 @@
+package _2_operators;
+
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println("Original Value: " + a);
+
+        System.out.println("Post Increment: " + a++);
+        System.out.println("After Post Increment: " + a);
+
+        System.out.println("Pre Increment: " + ++a);
+
+        System.out.println("Post Decrement: " + a--);
+        System.out.println("After Post Decrement: " + a);
+
+        System.out.println("Pre Decrement: " + --a);
+    }
+}
