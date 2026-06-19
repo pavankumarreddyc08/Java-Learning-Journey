426b633588abbd944aca05ce293e73f22b5fcc01
pavankumarreddyc08 <pavankumarreddyc08@gmail.com>
2026-06-19 22:30:31 +0530

    Added conditional statements examples

---
+++ src/_3_conditional_statements/NestedIf.java
@@ -0,0 +1,19 @@
+package _3_conditional_statements;
+
+public class NestedIf {
+    public static void main(String[] args) {
+
+        int age = 25;
+        boolean hasLicense = true;
+
+        if (age >= 18) {
+            if (hasLicense) {
+                System.out.println("Can drive");
+            } else {
+                System.out.println("Need a driving license");
+            }
+        } else {
+            System.out.println("Underage");
+        }
+    }
+}
---
+++ src/_3_conditional_statements/TernaryOperatorDemo.java
@@ -0,0 +1,12 @@
+package _3_conditional_statements;
+
+public class TernaryOperatorDemo {
+    public static void main(String[] args) {
+
+        int number = 15;
+
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(result);
+    }
+}
