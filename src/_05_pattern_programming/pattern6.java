798d5e3d2a16b87fec0195556a4ac8fcb4c1b1f5
pavankumarreddyc08 <pavankumarreddyc08@gmail.com>
2026-06-22 20:18:19 +0530

    added pattern programs

--- src/_05_pattern_programming/pattern6.java
+++ src/_05_pattern_programming/pattern6.java
@@ -3,9 +3,9 @@ package _05_pattern_programming;
 public class pattern6 {
     public static void main(String[] args) {
         int n=5;
-        for (int i = 0; i < n ; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.println((i+j)-1);
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print((i+j)-1 + " ");
             }
             System.out.println();
         }
